3ec3d9d398b1aa0d620067c3c0d7e20bc203dc8d
KauanMO <kauaanmatheus@gmail.com>
2024-05-15 13:55:33 -0300

    feat: initial commit in java

---
+++ src/test/java/com/suis/bootcamps/BootcampsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suis.bootcamps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
